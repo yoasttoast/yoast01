73f85d92921f9a7bfbe2e3e2fa3597570d85f5e7
Jacob Yoast <jacob.yoast@gmail.com>
2020-01-18 23:31:32 -0500

    First commit

---
+++ yoast_pa01/src/yoast_pa01/GoodbyeWorld.java
@@ -0,0 +1,8 @@
+package yoast_pa01;
+
+public class GoodbyeWorld {
+
+	public static void main(String[] args) {
+		System.out.printf("%s%n", "Goodbye, World!");
+	}
+}
---
+++ yoast_pa01/src/yoast_pa01/HelloWorld.java
@@ -0,0 +1,7 @@
+package yoast_pa01;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
